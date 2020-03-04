0f2f9b0c24b17187712b17a8f6494048ef9f1dde
jenjouhung <jenjou.hung@gmail.com>
2020-03-04 22:11:52 +0800

    修正完成時之表達方式

--- mytest.py
+++ mytest.py
@@ -32,15 +32,8 @@ def align(
 	else:
 		dcs="".join(set(list(refString)+list(qryString)))
 
-	#t1 = datetime.datetime.now()
-	#print ("  "*2+"找不重複字執行了：{:.7f} 秒".format((t1 - t0).microseconds*0.000001))
-
 	#處始化比對器
 	aligner = align_init(dcs)
-
-	#t2 = datetime.datetime.now()
-	#print ("  "*2+"初始化執行了：{:.7f} 秒".format((t1 - t1).microseconds*0.000001))
-
 
 	compareTaskQueue=[]  #用來存放比較工作的Queue
 
@@ -158,17 +151,17 @@ loop=0
 
 t0 = datetime.datetime.now()
 while (len(compareStringArray)):
-	# starttime = datetime.datetime.now()
+	#starttime = datetime.datetime.now()
 	refString,qryString = compareStringArray.pop()
 	loop+=1
 	#print("{},".format(loop),end="")
-	# endtime = datetime.datetime.now()
-	# print ("執行完成，花費：{:.7f} 秒".format((endtime-starttime).microsecond*0.000001))
+	#endtime = datetime.datetime.now()
+	#print ("執行完成，花費：{:.7f} 秒".format((endtime-starttime).microseconds*0.000001))
 	alignMessges = align(refString,qryString)
 
 
 t1= datetime.datetime.now()
-print ("執行完成，花費：{} 秒".format((t1-t0).seconds))
+print ("執行完成，花費：{} 秒".format((t1-t0).microseconds*0.000001))
 print ("-"*40)
 
 """
